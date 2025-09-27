967c0a1002cb3888cecaa252e6a12c0c4c5551df
saturdaymofever <saturdaymofever@gmail.com>
2025-09-27 08:44:05 +0200

    center_OK

--- level.gd
+++ level.gd
@@ -7,17 +7,18 @@ var tile_scene = preload("res://tile.tscn")
 var grid_offset = (tile_size * 3)/2
 var grid = Vector2(tile_size*3, tile_size*3)
 
-
-
+var margin_x = (screen_size.x - tile_size * 3 )/ 2
+var margin_y = (screen_size.y - tile_size * 3 )/ 2
 
 func _ready() -> void:
 	
-	for x in range(1):
-		for y in range(1):
+	for x in range(3):
+		for y in range(3):
 			var tile = tile_scene.instantiate()
-			tile.position = Vector2(tile_size * x, tile_size * y)
+			tile.position = Vector2(tile_size * x + margin_x, tile_size * y + margin_y)
 			add_child(tile)
 	print(grid_offset)
+
 
 	
 func _process(delta: float) -> void:
