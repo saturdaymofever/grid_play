7cb28c3ddce2d3de9dfc4a0b81f0b0b28f36c32b
saturdaymofever <saturdaymofever@gmail.com>
2025-09-29 19:57:20 +0200

    test

--- level.gd
+++ level.gd
@@ -1,23 +1,26 @@
 extends Node
 
-var screen_size = Vector2(1920, 1080)
-var screen_center = Vector2(screen_size.x / 2, screen_size.y / 2)
-var tile_size = 128
+var screen_size = Vector2(2560, 1440)
+var tile_size = 384
 var tile_scene = preload("res://tile.tscn")
-var grid_offset = (tile_size * 3)/2
-var grid = Vector2(tile_size*3, tile_size*3)
+var grid_scene = preload("res://grid_container.tscn")
+var hud_scene = preload("res://canvas_layer.tscn")
 
 var margin_x = (screen_size.x - tile_size * 3 )/ 2
 var margin_y = (screen_size.y - tile_size * 3 )/ 2
 
+
 func _ready() -> void:
 	
-	for x in range(3):
-		for y in range(3):
-			var tile = tile_scene.instantiate()
-			tile.position = Vector2(tile_size * x + margin_x, tile_size * y + margin_y)
-			add_child(tile)
-	print(grid_offset)
+#	for x in range(3):
+#		for y in range(3):
+#			var tile = tile_scene.instantiate()
+#			tile.position = Vector2(tile_size * x + margin_x, tile_size * y + margin_y)
+#			add_child(tile)
+	var grid = grid_scene.instantiate()
+	add_child(grid)
+	var hud = hud_scene.instantiate()
+	add_child(hud)
 
 
 	
