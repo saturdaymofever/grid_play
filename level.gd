cfe2a305d2f642f21ed3e18f4bd4a7707ba33a3d
saturdaymofever <saturdaymofever@gmail.com>
2025-09-29 21:09:37 +0200

    test

--- level.gd
+++ level.gd
@@ -4,14 +4,14 @@ var screen_size = Vector2(2560, 1440)
 var tile_size = 384
 var tile_scene = preload("res://tile.tscn")
 var grid_scene = preload("res://grid_container.tscn")
-var hud_scene = preload("res://canvas_layer.tscn")
+var hud_scene = preload("res://hud.tscn")
 
 var margin_x = (screen_size.x - tile_size * 3 )/ 2
 var margin_y = (screen_size.y - tile_size * 3 )/ 2
 
 
 func _ready() -> void:
-	
+	pass
 #	for x in range(3):
 #		for y in range(3):
 #			var tile = tile_scene.instantiate()
@@ -19,10 +19,13 @@ func _ready() -> void:
 #			add_child(tile)
 #	var grid = grid_scene.instantiate()
 #	add_child(grid)
-	var hud = hud_scene.instantiate()
-	add_child(hud)
+
 
 	
 func _process(delta: float) -> void:
 	pass
 	
+
+
+func _on_hud_start_exploring() -> void:
+	print("test")
