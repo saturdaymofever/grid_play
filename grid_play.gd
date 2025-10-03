67be2fae423c6e6400007f035622f0f995797455
saturdaymofever <saturdaymofever@gmail.com>
2025-10-03 10:22:54 +0200

    add commentary

--- grid_play.gd
+++ grid_play.gd
@@ -1,31 +1,38 @@
 extends Node
 
+# Ici on preload les scenes du player et du monstre, nous en auront besoin
 var player_scene = preload("res://scenes/player.tscn")
 var monster_scene = preload("res://scenes/monster.tscn")
 
+# Déclaration des variables qui recevront les instances plus haut,
+# on leur à declarer leur types, dans chaque scene nous avons un class_name 
 var player:Player
 var monster:Monster
 
+# nous comptons le nombre de monstre tué
 var monster_defeated:int = 0
+# nous fixons l'objectif pour gagner le jeu (state WIN)
 const win_objectif = 3
 
+# Nous definissons ici la taille de la grille, il s'agit d'une constante
 const grid_size:Vector2 = Vector2(3, 3)
-
+# Nous définissons une variable pour l'etat actuel du jeu en lien avec enum plus bas
 var current_state:State
-
+# Un enum des états
 enum State {
 	EXPLORATION,
 	FIGHT,
 	WIN,
 	GAME_OVER
 }
-
+# la fonction exploration 
 func process_exploration_turn():
 	player.new_rand_coord(grid_size)
 	monster.new_rand_coord(grid_size)
 	if player.coord == monster.coord:
 		current_state = State.FIGHT
 
+# la fonction combat 
 func process_fight_turn():
 	monster.take_damage(player.power)
 	player.take_damage(monster.power)
@@ -38,6 +45,7 @@ func process_fight_turn():
 		else:
 			current_state = State.EXPLORATION
 
+# ici on instantie et declare les variables de départ
 func _ready() -> void:
 	current_state = State.EXPLORATION
 	player = player_scene.instantiate()
